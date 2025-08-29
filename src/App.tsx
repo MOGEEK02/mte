2310cfc5d4753e1f35abfb0ea6b1ccf2e0e9fd7d
mogeek02 <moutie225@gmail.com>
2025-08-29 02:36:29 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import CompanyLogosShowcase from "./components/company";
 import AboutUs from "./components/about";
 import './assets/css/animate.css';
 import './assets/css/nice-select.css';
-import './assets/css/LineIcons.2.0.css';
-import './assets/css/bootstrap.4.5.2.min.css';
+
 import './assets/css/default.css';
 import './assets/css/style.css';
 import './App.css';
