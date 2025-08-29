a6db15cae7c78720b729d644c9a1a76d77b91353
mogeek02 <moutie225@gmail.com>
2025-08-29 03:22:23 +0100

    fix js

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import Navbar from "./components/Navbar";
 import MarqueeDemo from "./components/MarqueeDemo";
 import CompanyLogosShowcase from "./components/company";
 import AboutUs from "./components/about";
-import './assets/css/animate.css';
-import './assets/css/nice-select.css';
-
-import './assets/css/default.css';
-import './assets/css/style.css';
-import './App.css';
 import ExpertiseSection from "./components/ExpertiseSection";
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
