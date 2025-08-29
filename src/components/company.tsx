0d69b9d476dfe461c1b16c5a33551f9614c4f7a3
mogeek02 <moutie225@gmail.com>
2025-08-29 05:16:35 +0100

    v1.00

--- src/components/company.tsx
+++ src/components/company.tsx
@@ -6,7 +6,7 @@ const CompanyLogosShowcase = () => {
       <div className=" flex max-w-md lg:max-w-none w-auto lg:w-full flex-wrap  items-center justify-center lg:justify-between px-5">
         
         <div className="m-4 lg:mx-0 w-28 flex items-center justify-center">
-          <img src="./images/ABB.png" alt="ABB Logo" className="w-36 h-auto" />
+          <img src="/images/ABB.png" alt="ABB Logo" className="w-36 h-auto" />
         </div>
 
         <div className="m-4 lg:mx-0 w-28 flex items-center justify-center">
