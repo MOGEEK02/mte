fdb9dec6a3b51f03eb159a421e99543c71da646c
mogeek02 <moutie225@gmail.com>
2025-08-29 04:49:00 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import CompanyLogosShowcase from "./components/company";
 import AboutUs from "./components/about";
 import ExpertiseSection from "./components/ExpertiseSection";
 import Preloader from "./components/Preloader";
-import HeaderNavbar from "./components/HeaderNavbar";
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
 function App() {
   return (
