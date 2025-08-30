df33d3799e7c97ab5c09dcd4bc0112267202c034
mogeek02 <moutie225@gmail.com>
2025-08-30 19:13:52 +0100

    prog

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import CompanyLogosShowcase from "./components/company";
 import AboutUs from "./components/about";
 import ExpertiseSection from "./components/ExpertiseSection";
 import Preloader from "./components/Preloader";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import Footer from "./components/footer"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
